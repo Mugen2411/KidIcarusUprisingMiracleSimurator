af18f990bcbf7a33dac8fa46b48fa5d95d267c68
Mugen2411 <shojirintaro@gmail.com>
2025-09-14 13:55:21 +0900

    add: メインループを書いた

---
+++ MiracleSim/Source/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	while (1)
+	{
+		std::cout << "コマンドを入力してください > ";
+
+		char command;
+		std::cin >> command;
+
+		switch (command)
+		{
+		case 'q':
+		{
+			std::cout << "アプリケーションを終了します。" << std::endl;
+			exit(0);
+		}break;
+		default:
+		{
+			std::cout << "不明なコマンドです。hコマンドでヘルプを確認してください。" << std::endl;
+		}break;
+		}
+	}
+	return 0;
+}
