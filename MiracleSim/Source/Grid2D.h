3b5d34a371ae756eac7cc51928b5e2407e7360b6
Mugen2411 <shojirintaro@gmail.com>
2025-09-14 14:30:30 +0900

    add: 奇跡IDを追加

---
+++ MiracleSim/Source/Grid2D.h
@@ -0,0 +1,167 @@
+﻿#ifndef __INCLUDED_GRID2D__
+#define __INCLUDED_GRID2D__
+
+#include <vector>
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+
+/**
+    @brief    二次元のグリッドを表すクラス
+
+    @tparam    T   格納する型
+ */
+template <class T>
+class Grid2D
+{
+public:
+    /**
+        @brief    コンストラクタ
+
+        @param[in]    height          高さ
+        @param[in]    width           幅
+        @param[in]    initialValue    初期値
+    */
+    Grid2D(int64_t height, int64_t width, T initialValue)
+        : m_data(height * width, initialValue), m_width(width), m_height(height)
+    {
+    }
+
+    /**
+        @brief            指定した座標の値にアクセスする
+
+        @param[in]    x   X座標
+        @param[in]    y   Y座標
+
+        @return         目的の座標の参照
+     */
+    T &Ref(int64_t x, int64_t y)
+    {
+        return m_data[GetIndex(x, y)];
+    }
+
+    /**
+        @brief    高さの取得
+
+        @return 高さ
+     */
+    int64_t GetHeight() const
+    {
+        return m_height;
+    }
+
+    /**
+        @brief    幅の取得
+
+        @return 幅
+     */
+    int64_t GetWidth() const
+    {
+        return m_width;
+    }
+
+    /**
+        @brief    Indexの取得
+
+        @param[in]    x       X座標
+        @param[in]    y       Y座標
+
+        @return     Index
+     */
+    int64_t GetIndex(int64_t x, int64_t y) const
+    {
+        assert(IsInner(x, y));
+
+        return y * m_width + x;
+    }
+
+    /**
+        @brief        X座標を取得する
+
+        @param[in]    idx     インデックス
+
+        @return     X座標
+     */
+    int64_t GetX(int64_t idx) const
+    {
+        assert(0 <= idx && idx < m_height * m_width);
+
+        return idx % m_width;
+    }
+
+    /**
+        @brief        Y座標を取得する
+
+        @param[in]    idx     インデックス
+
+        @return     Y座標
+     */
+    int64_t GetY(int64_t idx) const
+    {
+        assert(0 <= idx && idx < m_height * m_width);
+
+        return idx / m_width;
+    }
+
+    /**
+        @brief            座標がグリッドの内部か否か
+
+        @param[in]    x       X座標
+        @param[in]    y       Y座標
+
+        @retval     true    グリッドの内部にいる
+        @retval     false   グリッドの外側
+     */
+    bool IsInner(int64_t x, int64_t y) const
+    {
+        return 0 <= x && x < m_width && 0 <= y && y < m_height;
+    }
+
+    /**
+        @brief        全てのマスに対して操作する
+
+        @tparam     Func    関数の型
+
+        @param[in]    func    操作する関数(引数としてidxとその地点の値が貰える)
+     */
+    template <class Func>
+    void ForEach(Func func)
+    {
+        for (int i = 0; i < m_width * m_height; ++i)
+        {
+            func(i, m_data[i]);
+        }
+    }
+
+private:
+    std::vector<T> m_data; //!< 本体となる配列
+    int64_t m_width;       //!< 幅
+    int64_t m_height;      //!< 高さ
+
+    template <class U>
+    friend std::istream &operator>>(std::istream &stream, Grid2D<U> &dest);
+};
+
+/**
+    @brief            入力ストリーム演算子
+
+    @tparam    T       Grid2Dに格納する型
+    @param[in]        stream  入力ストリーム
+    @param[in]        dest    入力を受け付ける先のGrid2D
+
+    @return                 入力ストリーム
+ */
+template <class T>
+std::istream &operator>>(std::istream &stream, Grid2D<T> &dest)
+{
+    for (int64_t y = 0; y < dest.m_height; ++y)
+    {
+        for (int64_t x = 0; x < dest.m_width; ++x)
+        {
+            stream >> dest.Ref(x, y);
+        }
+    }
+    return stream;
+}
+
+#endif //__INCLUDED_GRID2D__
---
+++ MiracleSim/Source/MiracleID.h
@@ -0,0 +1,84 @@
+﻿#pragma once
+
+/**
+ * @brief 奇跡ID
+ */
+class MiracleID
+{
+public:
+	/**
+	 * @brief デフォルトコンストラクタ
+	 */
+	MiracleID()
+		: m_id(INVALID)
+		, m_level(INVALID)
+	{
+	}
+
+	/**
+	 * @brief コンストラクタ
+	 * @param ID 奇跡ID
+	 * @param level レベル
+	 */
+	MiracleID(int ID, int level)
+		: m_id(ID)
+		, m_level(level)
+	{
+	}
+
+	/**
+	 * @brief	IDが有効かどうか
+	 * @return	有効ならtrue
+	 */
+	bool IsValid() const
+	{
+		return m_id != INVALID && m_level != INVALID;
+	}
+
+	/**
+	 * @brief 等価演算子
+	 * @param rhs 右辺
+	 * @return 右辺と等しいならtrue
+	 */
+	bool operator=(const MiracleID& rhs) const
+	{
+		return m_id == rhs.m_id && m_level == rhs.m_level;
+	}
+
+	/**
+	 * @brief 小なり演算子
+	 * @param rhs 右辺
+	 * @return 奇跡ID > レベルの優先度で小さいとする
+	 */
+	bool operator<(const MiracleID& rhs) const
+	{
+		if (m_id < rhs.m_id)
+		{
+			return true;
+		}
+		if (m_id > rhs.m_id)
+		{
+			return false;
+		}
+		if (m_level < rhs.m_level)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @brief int型へのキャストをサポート
+	 */
+	operator int() const
+	{
+		return m_id * MAX_LEVEL + m_level;
+	}
+public:
+	static constexpr int MAX_LEVEL = 4;	//!< レベルの最大値
+	static constexpr int INVALID = -1;	//!< 無効値
+
+private:
+	int m_id;		//!< 奇跡ID
+	int m_level;	//!< 奇跡レベル(1～4)
+};
